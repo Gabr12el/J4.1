1d2d826741f8752d11e7f813bfbb42326dd61d72
Gabr12el <98735582+Gabr12el@users.noreply.github.com>
2022-03-30 17:33:46 +0400

    2правка

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -30,8 +30,8 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
         // подготавливаем данные:
         long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
+        boolean registered = false;
+        long expected = 10;
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
         // производим проверку (сравниваем ожидаемый и фактический):
@@ -42,7 +42,7 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
         // подготавливаем данные:
         long amount = 1_000_000_60;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 500;
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
